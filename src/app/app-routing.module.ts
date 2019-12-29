1543a81f792375775b833f3171a505a50f72e4aa
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-12-29 05:22:31 -0300

    Adiciona rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { AboutComponent } from './pages/about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'calculator',
+    component: CalculatorComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    redirectTo: '/calculator',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
